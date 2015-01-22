60662b02b8db0b1756a484497996ba0156cbb26a
Kelsey Neil <kneil22@yahoo.com>
2015-01-22 15:51:13 -0500

    Updated .gitignore Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,29 @@
 /*global module:false*/
 
 module.exports = function(grunt) {
+	'use strict';
+
 	var sass_files = [
 		'components/sass/**/*.scss'
+	];
+	var js_files = [
+		'components/js/lib/**/*.js'
 	];
 
 	// Project configuration.
 	grunt.initConfig({
+		uglify: {
+			compress: {
+				files: [
+					{
+						expand: true, // enable dynamic expansion
+						cwd: 'components/js/lib/', // src matches are relative to this path
+						src: ['**/*.js'], // pattern to match
+						dest: 'components/js/min/'
+					}
+				]
+			}
+		},
 		compass: {
 			prod: {
 				config: 'config.rb',
@@ -14,15 +31,18 @@ module.exports = function(grunt) {
 			}
 		},
 		watch: {
-			files: sass_files,
+			files: js_files.concat( sass_files ),
 			tasks: [
-				'compass:prod'
+				'compass:prod',
+				'newer:uglify'
 			]
 		}
 	});
 
 	// Default task.
+	grunt.loadNpmTasks( 'grunt-newer' );
 	grunt.loadNpmTasks( 'grunt-contrib-compass' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.registerTask( 'default', ['compass:prod'] );
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask( 'default', ['newer:uglify', 'compass:prod'] );
 };
